ff519c874098eb5a9e1c6d99ca812ead8c222cb9
Markus Klotzbuecher <mk@mkio.de>
2020-05-25 13:23:49 +0200

    update to microblx-0.8
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- src/ubxkdl.hpp
+++ src/ubxkdl.hpp
@@ -1,8 +1,13 @@
+#ifndef UBXKDL_HPP
+#define UBXKDL_HPP
+
 #include <ubx/ubx.h>
 #include <kdl/frames.hpp>
 
-gen_class_accessors(KDLVector, KDL::Vector, struct kdl_vector);
-gen_class_accessors(KDLRotation, KDL::Rotation, struct kdl_rotation);
-gen_class_accessors(KDLFrame, KDL::Frame, struct kdl_frame);
-gen_class_accessors(KDLTwist, KDL::Twist, struct kdl_twist);
-gen_class_accessors(KDLWrench, KDL::Wrench, struct kdl_wrench);
+def_class_accessors(KDLVector, KDL::Vector, struct kdl_vector);
+def_class_accessors(KDLRotation, KDL::Rotation, struct kdl_rotation);
+def_class_accessors(KDLFrame, KDL::Frame, struct kdl_frame);
+def_class_accessors(KDLTwist, KDL::Twist, struct kdl_twist);
+def_class_accessors(KDLWrench, KDL::Wrench, struct kdl_wrench);
+
+#endif
